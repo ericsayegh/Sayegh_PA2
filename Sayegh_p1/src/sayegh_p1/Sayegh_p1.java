ad099a6016e9992f81d5ed533b9e0fe3ab85ed5d
Eric Sayegh <ericsayegh@knights.ucf.edu>
2019-09-21 16:27:58 -0400

    first commit

--- Sayegh_p1/src/sayegh_p1/Sayegh_p1.java
+++ Sayegh_p1/src/sayegh_p1/Sayegh_p1.java
@@ -1,22 +1,47 @@
-package Sayegh_p1;
+package sayegh_p1;
 
 import java.util.Scanner;
 
-public class problem_1 {
+public class Sayegh_p1 {
 
 	public static void main(String[] args) {
+
 		Scanner scnr = new Scanner (System.in);
 		int digit1, digit2, digit3, digit4;
 		int bdigit1, bdigit2, bdigit3, bdigit4;
+		int length;
 
 		System.out.println("please input the first digit of your code");
 		digit1 = scnr.nextInt();
+		
+		while( digit1 >= 10 ){
+			System.out.println("please input the first digit of your code");
+			digit1 = scnr.nextInt();
+		}
+	
 		System.out.println("please input the second digit of your code");
 		digit2 = scnr.nextInt();
+		
+		while( digit2 >= 10 ){
+			System.out.println("please input the second digit of your code");
+			digit2 = scnr.nextInt();
+		}
+		
 		System.out.println("please input the third digit of your code");
 		digit3 = scnr.nextInt();
+		
+		while( digit3 >= 10 ){
+			System.out.println("please input the third digit of your code");
+			digit3 = scnr.nextInt();
+		}
+		
 		System.out.println("please input the fourth digit of your code");
 		digit4 = scnr.nextInt();
+		
+		while( digit4 >= 10 ){
+			System.out.println("please input the fourth digit of your code");
+			digit4 = scnr.nextInt();
+		}
 		
 		System.out.println("You entered: " + digit1 + digit2 + digit3 + digit4 ); 
 		
@@ -28,7 +53,8 @@ public class problem_1 {
 
 		System.out.print("Encrpyted code is: " + bdigit1 + bdigit2 + bdigit3 + bdigit4 ); 
 		
-		
+
+
 	}
 
 }
